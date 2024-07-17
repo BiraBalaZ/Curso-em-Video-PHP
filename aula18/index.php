4b559df0c745101c60fe710bf4e6faeb38e1aa6b
BiraBalaZ <erick.m.anjos@gmail.com>
2024-07-17 13:32:05 -0300

    aula 18 e 19

--- aula18/index.php
+++ aula18/index.php
@@ -8,9 +8,83 @@
 </head>
 <body>
     <div>
+        <!-- <table border="1"><tr> -->
         <?php
+            ##### Array
+            // $n[0] = 3;
+            // $n[1] = 5;
+            // $n[2] = 8;
+            // $n[3] = 2;
+            // $n[] = 7; # o PHP entende que é para criar na última posição
+            // # ou
+            // $n = array(3, 5, 8, 2, 7);
+
+
+            ##### Criando por Range
+            // $c = range(5, 20, 5); # Cria um vetor chamado C com a primeira posição iniciada em 5, até 20 pulando de 5 em 5
+            // $c = [05][10][15][20]
+            //       0   1   2   3
+            // print_r($c);
+
+            // $c = range(5, 20, 2); # Pulando de 2 em 2
+            // $c = [05][07][09][11][13][15][17][19]
+            //       0   1   2   3   4   5   6   7
+            // print_r($c);
+
+
+            ##### Estrutura foreach !
+            #### para usar esse trecho, descomentar o <table> que envelopa o <?php>
+            // $c = range(5, 25, 2);
+            // foreach($c as $valor) { # para cada valor no vetor $c
+            //     echo "<td>$valor";
+            // }
+
+
+            ##### Chaves Personalizadas
+            // $vetor = array(1=>"A", 3=>"B", 6=>"C", 8=>"D");
+            # ou
+            // $vetor = array( 3 => 5,
+            //                 1 => 9,
+            //                 0 => 8,
+            //                 7 => 7);
+            // $vetor[] = "E";
+            // unset($vetor[3]); # Desalocar o Vetor na posição 3
+            // print_r($vetor);
+
+
+            #### para usar esse trecho, descomentar o <table> que envelopa o <?php>
+            // foreach($vetor as $valor) { # para cada valor no vetor $c
+            //         echo "<td>$valor";
+            // }
             
+
+            ##### Chaves Associativas
+            // $cadastro = array("nome" => "Ana",  # String
+            //                   "idade" => 23,    # Int
+            //                   "peso" => 78.5);  # Float
+            // $cadastro["fuma"] = true;           # Boolean
+
+            // print_r($cadastro);
+
+            # utilizando a estrutura foreach com esses valores
+            // foreach($cadastro as $campo => $valor) {
+            //     echo "O valor do campo <span class='foco'>$campo</span> possui o valor <span class='foco'>$valor</span><br>";
+            // }
+
+
+            ##### Matrizes em PHP
+            ### Em resumo, elas não existem, você criar arrays dentro de arrays
+            $n = array(array(2,3),      # 0 [2][3]
+                       array(3,4),      # 1 [3][4]
+                       array(9,5));     # 2 [9][5]
+                                        #    0  1
+
+            $n[2][0] = $n[1][1]         # 0  [2] [3]
+                                        # 1  [3] [4]
+                                        # 2 [>4<][5]
+                                        #     0   1
         ?>
+        <!-- </table> -->
     </div>
 </body>
 </html>
--- aula19/index.php
+++ aula19/index.php
@@ -8,9 +8,93 @@
 </head>
 <body>
     <div>
-        <?php
-        
-        ?>
+        <pre>
+            <?php
+                ##### Exibindo vetor
+                ### a tag <pre> deixa pré-formatado
+                // $n = array(3, 5, 8, 2); # [3][5][8][2]
+                //                         #  0  1  2  3
+                // print_r($n);
+                // var_dump($n); # melhor para ver o tipo dos elementos, int, str... e a quantidade de elementos
+
+                // $contador = count($n); # conta a quantidade de elementos
+                // echo ("O vetor tem $contador elementos");
+
+                ##### Adicionando valor no FINAL do vetor
+                // $v = array("A", "J", "M", "X", "K");
+                // print_r($v);
+
+                // $v[] = "O";
+                // # ou
+                // array_push($v, "L");
+                
+                // print_r($v);
+
+                // ### Eliminando o ÚLTIMO item do vetor
+                // array_pop($v);
+                // print_r($v);
+
+
+                // ##### Adicionando valor no INÍCIO do vetor
+                // array_unshift($v, "T");
+                // print_r($v);
+
+                // ### Eliminando o PRIMEIRO item do vetor
+                // array_shift($v);
+                // print_r($v);
+
+
+                ##### Ordenação Associativa - asort() - Menor para o maior
+                // $n = array(3, 5, 8, 2);
+
+                // # Antes     [3][5][8][2]
+                // #            0  1  2  3
+                // asort($n); # mexe inclusive nos íncices
+                
+                // # Depois    [2][3][5][8]
+                // #            3  0  1  2
+                // print_r($n);
+
+
+                ##### Ordenação Associativa Reversa - arsort() - Maior para o menor
+                // $n = array(3, 5, 8, 2);
+
+                // # Antes     [3][5][8][2]
+                // #            0  1  2  3
+                // arsort($n);
+                
+                // # Depois    [8][5][3][2]
+                // #            2  1  0  3
+                // print_r($n);
+
+
+                ##### Ordenando por Chaves - ksort() - Menor para o maior
+                $v = array(2=>"A", 5=>"J", 0=>"M", 3=>"X", 4=>"K");
+                print_r($v);
+                ksort($v);
+                print_r($v);
+
+                # Antes
+                # [A][J][M][X][K]
+                #  2  5  0  3  4
+                #
+                # Depois
+                # [M][A][X][K][J]
+                #  0  2  3  4  5
+
+                ##### Ordenando por Chaves Reversa - krsort() - Maior para o maior
+                krsort($v);
+                print_r($v);
+
+                # Antes
+                # [M][A][X][K][J]
+                #  0  2  3  4  5
+                #
+                # Depois
+                # [J][K][X][A][M]
+                #  5  4  3  2  0
+            ?>
+        </pre>
     </div>
 </body>
 </html>
