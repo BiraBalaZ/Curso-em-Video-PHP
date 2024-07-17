c15b574232a702b6930a057766cef3f27e0de617
BiraBalaZ <erick.m.anjos@gmail.com>
2024-07-17 12:05:18 -0300

    aula 16 e 17

--- aula16/index.php
+++ aula16/index.php
@@ -9,7 +9,71 @@
 <body>
     <div>
         <?php
+            ##### printf()
+            // $produto = "leite";
+            // $preco = 4;
+            // // printf("O %s está custando R$ %.2f",$produto, $preco);
+
+            ###### print_r()
+            // $vetor[0] = 4;
+            // $vetor[1] = 8;
+            // $vetor[2] = 3;
+            // // print_r($vetor); 
+
+            // $vetor2 = array(3, 7, 6, 2, 1, 9);
+            // print_r($vetor2);
+
+            ###### wordwrap()
+            // $txt = "Aqui nós temos um exemplo de string gigante que será quebrada em alguns lugares e nao sei mais o que escrever vou colocar lorem ipsum dolor sit amet";
+            // $res = wordwrap($txt, 5, "<br>\n", false);
+            // echo $res;
+
+            ###### strlen() - Conta quandos caracteres tem na string incluindo espaços
+            // $txt = "Curso em Vídeo PHP";
+            // $tamanho = strlen($txt);
+            // echo $tamanho;
+
+            #####  trim() - Elimina espaços do início e fim da String 
+            ##### ltrim() - Elimina espaços do inicio (left)
+            ##### rtrim() - Elimina espaços do fim (right)
+            // $nome = "   José da Silva   ";
+            // echo strlen(trim($nome));
+
+            ##### str_word_count() - Conta as palavras de uma String
+            // $frase = "Eu vou estudar PHP";
+            // $count = str_word_count($frase, 0);
+            // echo $count;                             # 4
+            // $count = str_word_count($frase, 1);
+            // print_r ($count);                        # Array ( [0] => Eu [1] => vou [2] => estudar [3] => PHP )
+            
+            // $count = str_word_count($frase, 2);
+            // print_r ($count);                        # Array ( [0] => Eu [3] => vou [7] => estudar [15] => PHP )
+
+            ##### explode() - Versao mais nova do String Word Count - str_word_count()
+            // $site = "Curso em Vídeo";
+            // $vetor = explode(" ", $site);
+            // print_r ($vetor);                        # Array ( [0] => Curso [1] => em [2] => Vídeo )
         
+            ##### str_split() - Pega cada Letra e coloca em um índice
+            // $nome = "Maria";
+            // $vetor = str_split($nome);
+            // print_r($vetor);                          # Array ( [0] => M [1] => a [2] => r [3] => i [4] => a )
+
+            ##### implode() || join() - Junta os índices em uma string com separador, nesse caso, uma "#"
+            // $vetor[0] = "Curso";
+            // $vetor[1] = "em";
+            // $vetor[2] = "Vídeo";
+            // $texto = implode("#", $vetor);
+            // print($texto);                            # Curso#em#Vídeo
+
+            ##### chr()
+            $letra = chr(67);
+            echo "A letra de código 67 é $letra.";
+
+            ##### ord()
+            $letra = "C";
+            $codigo = ord($letra);
+            echo "O código da letra $letra é $codigo.";
         ?>
     </div>
 </body>
--- aula17/index.php
+++ aula17/index.php
@@ -9,7 +9,74 @@
 <body>
     <div>
         <?php
-        
+            ##### strtoloer() - Deixa a String toda em letras minúsculas
+            // $nome = "Erick Monteiro";
+            // print("Seu nome é ".strtolower($nome));
+
+            ##### strtolupper() - Deixa a String toda em letras maiúsculas
+            // $nome = "Erick Monteiro";
+            // print("Seu nome é ".strtoupper($nome));
+
+            ##### ucfirst() - Coloca a primeira letra em maiúsucla
+            // $nome = "erick monteiro";
+            // print("Seu nome é ".ucfirst($nome));
+
+            ##### lcfirst() - Coloca a primeira letra em minúsucla
+            // $nome = "ERICK MONTEIRO";
+            // print("Seu nome é ".lcfirst($nome));
+
+            ##### ucwords() - Deixa todas as primeiras letras em maiúsucula
+            // $nome = "erick monteiro";
+            // print("Seu nome é ".ucwords($nome));
+
+            ##### strpos() - Mostra a posição que a String solicitada foi encontrada
+            // $frase = "Estou aprendendo PHP";
+            // $pos = strpos($frase, "PHP");
+            // echo $pos; # 17
+
+            ##### stripos() - Encontra mesmo se estiver em minúsculo
+            // $frase = "Estou aprendendo PHP";
+            // $pos = stripos($frase, "php");
+            // echo $pos; # 17
+
+            ##### substr_count() - Mostra quantas vezes a string apareceu dentro do texto
+            // $frase = "Estou aprendendo PHP no Curso em Vídeo de PHP";
+            // $contador = substr_count($frase, "PHP");
+            // echo $contador; # 2
+
+            ##### substr() - Fatia a string da posição inicial à final
+            // $site = "Curso em Vídeo";
+            // $sub = substr($site, 0, 5);
+            // echo $sub; # Curso
+            
+            // $sub = substr($site, 7); # da 7 até o final da string
+            // echo $sub; # m Vídeo
+
+            // $sub = substr($site, -5); # últimas 5 letras da string
+            // echo $sub; # Vídeo
+            
+            // $sub = substr($site,-5 , 2); # das últimas 5 letras da string pega a segunda letra
+            // echo $sub; # Vídeo
+
+            ##### str_pad() - complementa
+            // $nome = "Erick";
+            // $novo_nome = str_pad($nome, 30, "&nbsp", STR_PAD_RIGHT);
+            // echo "O $novo_nome é lindo!";
+
+            # STR_PAD_LEFT   - xxxxxxString - Espaços à esquerda.
+            # STR_PAD_CENTER - xxxStringxxx - Espaços ao redor.
+            # STR_PAD_RIGHT  - Stringxxxxxx - Espaços à direita.
+
+            ##### str_repeat() - Repete o texto
+            // $txt = str_repeat("PHP", 5);
+            // echo str_repeat("-", 40); # --------------------
+            // echo "<br>$txt<br>";      # PHPPHPPHPPHPPHP
+            // echo str_repeat("-", 40); # --------------------
+
+            ##### str_replace() - Substitui algo na string
+            // $frase = "Gosto de estudar Matemática!!!";
+            // $nova_frase = str_replace("Matemática", "PHP", $frase);
+            // echo $nova_frase;
         ?>
     </div>
 </body>
